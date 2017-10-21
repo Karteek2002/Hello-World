d56f0ada28f07c9bc3eab8df91a7bc1996d5a120
Raman <raman.sriram1@gmail.com>
2017-10-21 12:16:59 +0530

    deleted files

--- workspace/Core Java/src/com/core/basic/Test4.java
+++ workspace/Core Java/src/com/core/basic/Test4.java
@@ -9,15 +9,15 @@ public class Test4 {
 	}
 	@BeforeTest
 	public void b1(){
-		System.out.println("b1");
+		System.out.println("b133333333333");
 	}
 	@BeforeSuite
 	public void c1(){
-		System.out.println("c1");
+		System.out.println("c1111111111111");
 	}
 	@AfterTest
 	public void d1(){
-		System.out.println("d1");
+		System.out.println("d11111111111111");
 	}
 
 }
